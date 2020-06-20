1c4104dd345d820797570a432b9b8b77543e3a30
zero-cool <joaoszabo@gmail.com>
2020-06-20 05:24:58 -0300

    lodash

--- main.ts
+++ main.ts
@@ -2,18 +2,52 @@ import { join } from "https://deno.land/std/path/mod.ts";
 import { BufReader } from "https://deno.land/std/io/bufio.ts";
 import { parse } from "https://deno.land/std/encoding/csv.ts";
 
-async function loadPlanetsData ()  {
-    const path = join(".","original.csv")
-    
-    const file = await Deno.open(path);
-    const bufReader = new BufReader(file);
-    const result = await parse(bufReader, {
-        header: true,
-        comment: "#",
-    });
-    Deno.close(file.rid);
+import _ from "https://deno.land/x/deno_lodash/mod.ts";
 
-    console.log(result);
+interface Planet { 
+    [ key: string ] : string
 }
-await loadPlanetsData()
 
+async function loadPlanetsData() {
+  const path = join(".", "original.csv");
+
+  const file = await Deno.open(path);
+  const bufReader = new BufReader(file);
+
+  const result = await parse(bufReader, {
+    header: true,
+    comment: "#",
+  });
+
+  Deno.close(file.rid);
+
+  const planets = (result as Array<Planet>).filter((planet) => {
+    const planetaryRadius = Number(planet["koi_prad"]);
+    const stellarMass = Number(planet["koi_smass"]);
+    const stellarRadius = Number(planet["koi_srad"]);
+
+
+    return planet["koi_disposition"] === "CONFIRMED"
+    && planetaryRadius > 0.5 && planetaryRadius < 1.5
+    &&  stellarMass > 0.78 && stellarMass < 1.04
+    && stellarRadius > 0.99 && stellarRadius < 1.01;
+      
+  });
+
+  return planets.map((planet) => {
+      return _.pick(planet, [
+        "koi_prad",
+        "koi_smass",
+        "koi_srad",
+        "kepler_name",
+        "koi_count",
+        "koi_steff"
+      ]);
+  });
+}
+
+const newEarths = await loadPlanetsData();
+for (const planet of newEarths) {
+    console.log(planet)
+}
+console.log(`${newEarths.length} habitable planets`);
